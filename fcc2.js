a250699a6fbcee43a784069fadc1ff77519e32fa
PJ Angeloni <pj.angeloni@gmail.com>
2023-08-08 15:31:31 -0400

    added some temp files for studying

--- fcc2.js
+++ fcc2.js
@@ -2,7 +2,8 @@ let obj = {
   name: "FreeCodeCamp",
   review: "Awesome",
 };
-// Object.freeze(obj);
+Object.freeze(obj);
 obj.review = "bad";
 obj.newProp = "Test";
 console.log(obj);
+console.log(obj.newProp);
