6c23a188415005512ca43726782c53586467f7c0
Paolo <pj.angeloni@gmail.com>
2024-05-29 17:15:19 -0400

    completed icons and text

--- Courses/LearnReact/BusinessCard/App.jsx
+++ Courses/LearnReact/BusinessCard/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import AboutInterests from './Components/Interests';
+import AboutInterests from './Components/AboutInterests';
+import Footer from './Components/Footer';
 
 export default function App() {
     return (
         <main className='main-card'>
             <AboutInterests />
+            <Footer />
         </main>
     )
 }
