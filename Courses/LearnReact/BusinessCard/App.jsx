9fcb20170dade02d558917beb466250c17d413fa
Paolo <pj.angeloni@gmail.com>
2024-05-29 20:55:23 -0400

    business card complete

--- Courses/LearnReact/BusinessCard/App.jsx
+++ Courses/LearnReact/BusinessCard/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Info from './Components/Info'
 import AboutInterests from './Components/AboutInterests';
 import Footer from './Components/Footer';
 
 export default function App() {
     return (
         <main className='main-card'>
+            <Info />
             <AboutInterests />
             <Footer />
         </main>
